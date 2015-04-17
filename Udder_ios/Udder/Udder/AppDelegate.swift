9e858932c81af97ead7d3717a9f682b9fb135bbd
Benjamin Hendricks <benjaminhendricks@wifi-131-179-34-220.host.ucla.edu>
2015-04-17 13:07:17 -0700

    added Parse and Facebook SDKs, and initizliaed workspace

--- Udder_ios/Udder/Udder/AppDelegate.swift
+++ Udder_ios/Udder/Udder/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.setApplicationId("sIZN7Eo4sl6tR5ZdI04qIEKf5wm1QJN92jBxTLKb", clientKey: "IfKhgzcCazKuLPJCrQJwhDavQPTX59G0fo91bvuf")
+
         return true
     }
 
