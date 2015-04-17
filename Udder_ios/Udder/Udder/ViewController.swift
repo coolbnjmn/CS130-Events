6836519b50398ade1abdfb5035d7a65ac6664c96
Benjamin Hendricks <benjaminhendricks@wifi-131-179-34-220.host.ucla.edu>
2015-04-17 12:48:01 -0700

    created Udder app with ios8 target + cocoapods

---
+++ Udder_ios/Udder/Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Benjamin Hendricks on 4/17/15.
+//  Copyright (c) 2015 UCLA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
