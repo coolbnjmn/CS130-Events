14c1f272e2cd16bb6660b597a00883a5216f4f6c
Collin Yen <collinyen7@gmail.com>
2015-04-18 01:59:35 -0700

    Added segment control to switch between description and attendees in event view

--- Udder_ios/Udder/Udder/ViewControllers/EventViewController.swift
+++ Udder_ios/Udder/Udder/ViewControllers/EventViewController.swift
@@ -8,29 +8,60 @@
 
 import UIKit
 
+struct Segment {
+    static let kSegmentInfo = 0;
+    static let kSegmentAttendees = 1;
+}
+
 class EventViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var infoTableView: UITableView!
+    @IBOutlet weak var attendeesTableView: UITableView!
+    
+    
+    var segmentSelected = Segment.kSegmentInfo;
     
     override func viewDidLoad() {
         let titleViewNib:UINib = UINib(nibName: "EventViewTitleCell", bundle: nil);
-        self.tableView.registerNib(titleViewNib, forCellReuseIdentifier: Constants.CellIdentifiers.kEventTitleCell);
+        self.infoTableView.registerNib(titleViewNib, forCellReuseIdentifier: Constants.CellIdentifiers.kEventTitleCell);
         
-        self.tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 0));
+        self.infoTableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 0));
         self.edgesForExtendedLayout = UIRectEdge.None;
     }
     
-    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 180;
-    }
-    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1;
+        return 5;
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = self.tableView.dequeueReusableCellWithIdentifier(Constants.CellIdentifiers.kEventTitleCell, forIndexPath: indexPath) as EventViewTitleCell;
+//        let cell = self.tableView.dequeueReusableCellWithIdentifier(Constants.CellIdentifiers.kEventTitleCell, forIndexPath: indexPath) as EventViewTitleCell;
+        //let cell = self.tableView.dequeueReusableCellWithIdentifier("Identifier", forIndexPath: indexPath) as UITableViewCell;
+        let cell = UITableViewCell();
         
+        if  (tableView == self.infoTableView) {
+            cell.textLabel?.text = "info";
+            cell.imageView?.image = UIImage(named: "Beach.jpg");
+        }
+        else {
+            cell.textLabel?.text = "attendee";
+        }
+
         return cell;
     }
+    
+    @IBAction func viewSwitched(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+            case Segment.kSegmentInfo:
+                infoTableView.hidden = false;
+                attendeesTableView.hidden = true;
+                break;
+            case Segment.kSegmentAttendees:
+                infoTableView.hidden = true;
+                attendeesTableView.hidden = false;
+                break;
+            default:
+                break;
+        }
+    }
+    
 }
