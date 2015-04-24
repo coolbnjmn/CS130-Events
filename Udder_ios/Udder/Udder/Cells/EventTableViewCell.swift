0174dba488b6f668e16bffd6b3f9f294b10e959a
Benjamin Hendricks <benjaminhendricks@wifi-131-179-34-129.host.ucla.edu>
2015-04-23 20:36:51 -0700

    work on eventtableviewcell, about to add attributed label pod for ellipsisied text

--- Udder_ios/Udder/Udder/Cells/EventTableViewCell.swift
+++ Udder_ios/Udder/Udder/Cells/EventTableViewCell.swift
@@ -10,15 +10,55 @@ import UIKit
 
 class EventTableViewCell: UITableViewCell {
 
+    
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var eventTitleLabel: UILabel!
+    @IBOutlet weak var timeLocationSubview: UIView!
+    @IBOutlet weak var timeImageView: UIImageView!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var locationImageView: UIImageView!
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    func eventTableViewCellInit(params: NSDictionary) {
+        self.eventTitleLabel?.text = (params.objectForKey("title") as! String)
+        self.timeLabel?.text = (params.objectForKey("time") as! String)
+        self.locationLabel?.text = (params.objectForKey("location") as! String)
+        self.backgroundImageView?.image = UIImage(named:"placeholder.png")
+        self.locationImageView?.image = UIImage(named: "placeholder.png")
+        self.timeImageView?.image = UIImage(named: "placeholder.png")
+        self.categoryImageView?.image = UIImage(named: "placeholder.png")
+        
+        
+        self.eventTitleLabel.textColor = UIColor.whiteColor()
+        self.timeLocationSubview.backgroundColor = UIColor.clearColor()
+        self.timeLabel.textColor = UIColor.whiteColor()
+        self.locationLabel.textColor = UIColor.whiteColor()
+        self.eventTitleLabel.font = UIFont(name: "Avenir-Book", size:24)
+        self.timeLabel.font = UIFont(name: "Avenir-Book", size:18)
+        self.locationLabel.font = UIFont(name: "Avenir-Book", size: 18)
+        
+        self.timeLabel.sizeToFit()
+        self.locationLabel.sizeToFit()
+        
+        let maxTimeLabelWidth : CGFloat = self.timeLocationSubview.frame.size.width / 3
+        let maxLocationLabelWidth : CGFloat = 2*maxTimeLabelWidth
+        
+        if(self.timeLabel.frame.size.width > maxTimelabelWidth) {
+            
+        }
     }
     
 }
