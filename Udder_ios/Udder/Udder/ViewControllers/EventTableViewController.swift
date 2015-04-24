d4ececc114f6a3f44c6b491727eeadb9ebac8245
Benjamin Hendricks <benjaminhendricks@KirbyMaster.local>
2015-04-24 13:42:00 -0700

    integrated mark's flickr changes

--- Udder_ios/Udder/Udder/ViewControllers/EventTableViewController.swift
+++ Udder_ios/Udder/Udder/ViewControllers/EventTableViewController.swift
@@ -59,7 +59,7 @@ class EventTableViewController: UITableViewController {
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 2
+        return 5
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
