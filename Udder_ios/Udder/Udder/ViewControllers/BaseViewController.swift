7618bfe5c7407be88d2d916acc154930ccf4201f
Benjamin Hendricks <benjaminhendricks@wifi-131-179-34-181.host.ucla.edu>
2015-05-07 17:09:02 -0700

    added transition to invite people to event detail

--- Udder_ios/Udder/Udder/ViewControllers/BaseViewController.swift
+++ Udder_ios/Udder/Udder/ViewControllers/BaseViewController.swift
@@ -12,7 +12,7 @@ class BaseViewController: UIViewController, ViewControllerProtocolDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
 
