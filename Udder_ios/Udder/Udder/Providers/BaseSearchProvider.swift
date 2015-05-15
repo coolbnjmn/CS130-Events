bda313ef3313a548eea2899fc133b22e28ae52f9
Benjamin Hendricks <benjaminhendricks@KirbyMaster.local>
2015-05-15 14:22:43 -0700

    changed cancel button to be white

--- Udder_ios/Udder/Udder/Providers/BaseSearchProvider.swift
+++ Udder_ios/Udder/Udder/Providers/BaseSearchProvider.swift
@@ -28,6 +28,10 @@ class BaseSearchProvider: BaseProvider, UISearchResultsUpdating, UISearchControl
             controller.hidesNavigationBarDuringPresentation = false
             controller.searchBar.searchBarStyle = .Minimal
             controller.delegate = self
+            controller.searchBar.backgroundColor = Constants.Colors.ThemeColor
+            
+            var textFieldInsideSearchBar = controller.searchBar.valueForKey("searchField") as? UITextField
+            textFieldInsideSearchBar?.textColor = UIColor.whiteColor()
             
             self.tableView?.tableHeaderView = controller.searchBar
             return controller;
@@ -46,6 +50,7 @@ class BaseSearchProvider: BaseProvider, UISearchResultsUpdating, UISearchControl
 
     func updateSearchResultsForSearchController(searchController: UISearchController)
     {
+        
         self.applySearch(self)
         if let searchResults = self.searchResults {
             if(searchResults.count == 0 && count(searchController.searchBar.text) == 0) {
@@ -59,18 +64,29 @@ class BaseSearchProvider: BaseProvider, UISearchResultsUpdating, UISearchControl
         self.tableView?.reloadData()
     }
     
+    func didPresentSearchController(searchController: UISearchController) {
+        let uiButton = searchController.searchBar.valueForKey("cancelButton") as? UIButton
+        uiButton?.titleLabel?.textColor = UIColor.whiteColor()
+    }
+    
     func willPresentSearchController(searchController: UISearchController) {
         println("will present search controller")
+        
         if let searchResults = self.searchResults {
             self.searchProvider?.configure(self.searchResults!)
         } else {
-            self.searchProvider?.configure(self.data!)
+            if let data = self.data {
+                self.searchProvider?.configure(self.data!)
+            } else {
+                self.searchProvider?.configure(NSMutableArray())
+            }
         }
         self.searchProvider!.delegate = self.delegate
         self.tableView?.dataSource = self.searchProvider
         self.tableView?.delegate = self.searchProvider
         self.tableView?.reloadData()
     }
+    
     func willDismissSearchController(searchController: UISearchController) {
         println("will dismiss search controller")
         self.tableView?.dataSource = self.tableViewProvider
